8ba277223d972afa3b3135522b344c12125d2149
jmsilvas <jmsilva10@uc.cl>
2021-04-07 20:50:10 -0400

    fixes in character info

--- t1/src/components/Character.js
+++ t1/src/components/Character.js
@@ -9,10 +9,10 @@ class character extends Component {
                     img: "",
                     status:"",
                     nickname: "",
-                    appearence: [],
+                    appearance: [],
                     portrayed: "",
                     category: "",
-                    better_call_saul_appearence: []
+                    better_call_saul_appearance: []
                 }
         this.handleClick = this.handleClick.bind(this)
     }
@@ -32,10 +32,10 @@ class character extends Component {
                     img: result[0].img,
                     status:result[0].status,
                     nickname: result[0].nickname,
-                    appearence: result[0].appearence,
+                    appearance: result[0].appearance,
                     portrayed: result[0].portrayed,
                     category: result[0].category,
-                    better_call_saul_appearence: result[0].better_call_saul_appearence}
+                    better_call_saul_appearance: result[0].better_call_saul_appearance}
                 )
             }
         )
@@ -50,13 +50,19 @@ class character extends Component {
                 <div className="pop-up-window">
                     <div className="pop-up-content">
                         <button className="close-button" onClick={this.handleClick}>x</button>
+                        <img src={this.state.img} alt="character loading.."></img>
                         <div className="character-info">
-                            <div>
-                            <img src={this.state.img} alt="character image"></img>
-                            </div>
-                            {this.state.name}
-                           
-                            
+                            <div>Nombre: {this.state.name}</div>
+                            <div>Ocupación: {this.state.occupation}</div>
+                            <div>Estado: {this.state.status}</div>
+                            <div>Apodo: {this.state.nickname}</div>
+                            <div>Actor(a): {this.state.portrayed}</div>    
+                            <div>Series: {this.state.category}</div>
+                            <div>Temporadas en Breaking Bad: </div>
+                            <div>{this.state.appearance.map((app,index)=>(<button className="appearances-button">{app}</button>))}</div>
+                            <div>Temporadas Better Call Saul:</div>
+                            <div>{this.state.better_call_saul_appearance.map((app,index)=>(<button className="appearances-button">{app}</button>))}</div>          
+                    
                         </div>
                     </div>
                 </div>
